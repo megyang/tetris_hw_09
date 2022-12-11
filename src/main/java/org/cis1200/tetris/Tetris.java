4b5d0765275baa71245c07e2dff8d9933bfda72e
zhong yang <yangz100@hotmail.com>
2022-12-11 15:51:24 -0500

    Property file

--- src/main/java/org/cis1200/tetris/Tetris.java
+++ src/main/java/org/cis1200/tetris/Tetris.java
@@ -5,13 +5,19 @@ import org.cis1200.FileLineIterator;
 import javax.swing.*;
 import java.awt.*;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.util.List;
+import java.util.Properties;
+
+import org.cis1200.tetris.ReadProperties;
 
 public class Tetris implements Runnable {
     private static GameForm gf;
     private static StartupForm sf;
     private static LeaderboardForm lf;
-    public static final String PATH_TO_FALLEN = "files/fallenblocks.csv";
+    private static String CONFIG = ".properties";
+    public static String PATH_TO_FALLEN = "files/fallenblocks.csv";
+    public static String LEADERBOARD_PATH = "files/leaderboard";
 
 
     public static void start() {
@@ -57,5 +63,19 @@ public class Tetris implements Runnable {
         sf.setup();
         sf.setVisible(true);
         gf.setVisible(true);
+
+
+        Properties props = null;
+        try {
+            props = ReadProperties.readConfigFile(CONFIG);
+            PATH_TO_FALLEN = props.getProperty("PATH_TO_FALLEN");
+            LEADERBOARD_PATH = props.getProperty("LEADERBOARD_PATH");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            System.out.println("\nCouldn't open .properties. Using Default configuration");
+        }
+
+
     }
+
 }
